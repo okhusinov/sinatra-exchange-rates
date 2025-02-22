bd0f8dad4edd3078b33aefd44c5867680c66c822
okhusinov <okhusinov@chicagobooth.edu>
2025-02-22 17:48:14 +0000

    final

--- app.rb
+++ app.rb
@@ -16,4 +16,27 @@ get("/") do
 
 end
 
-puts @parsed_data
+get("/:from_currency") do
+  @the_symbol = params.fetch("from_currency")
+
+  exchange_rates_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY")}"
+  @raw_response = HTTP.get(exchange_rates_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+  @currencies = @parsed_data.fetch("currencies")
+
+  erb(:step_one)
+end
+
+get("/:from_currency/:to_currency") do
+  @from = params.fetch("from_currency")
+  @to = params.fetch("to_currency")
+
+  exchange_rates_url = "https://api.exchangerate.host/convert?from=#{@from}&to=#{@to}&amount=1&access_key=#{ENV.fetch("EXCHANGE_RATES_KEY")}"
+  @raw_response = HTTP.get(exchange_rates_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+  @amount = @parsed_data.fetch("result")
+
+  erb(:step_two)
+end
