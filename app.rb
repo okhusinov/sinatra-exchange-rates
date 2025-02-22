3719bf1c91d8dad6898f2fbfe68598fa18b5b914
okhusinov <okhusinov@chicagobooth.edu>
2025-02-22 17:05:08 +0000

    mid

--- app.rb
+++ app.rb
@@ -4,15 +4,16 @@ require "http"
 require "json"
 require "dotenv/load"
 
-exchange_rates_api_key = ENV.fetch("EXCHANGE_RATES_KEY")
-exchange_rates_url = "https://api.exchangerate.host/list?access_key=#{exchange_rates_api_key}"
-raw_exchange_rates_response = HTTP.get(exchange_rates_url)
-parsed_exchange_rates_response = JSON.parse(raw_exchange_rates_response)
-
-
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  exchange_rates_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY")}"
+  @raw_response = HTTP.get(exchange_rates_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+  @currencies = @parsed_data.fetch("currencies")
+
+  erb(:homepage)
+
 end
+
+puts @parsed_data
