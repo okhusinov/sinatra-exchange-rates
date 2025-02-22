3919813a2246aea2186171d8dcb5e393af9af927
okhusinov <okhusinov@chicagobooth.edu>
2025-02-22 15:28:10 +0000

    initial

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require "dotenv/load"
+
+exchange_rates_api_key = ENV.fetch("EXCHANGE_RATES_KEY")
+exchange_rates_url = "https://api.exchangerate.host/list?access_key=#{exchange_rates_api_key}"
+raw_exchange_rates_response = HTTP.get(exchange_rates_url)
+parsed_exchange_rates_response = JSON.parse(raw_exchange_rates_response)
+
 
 get("/") do
   "
